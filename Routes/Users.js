76d11462c5d8da60bfeaeb503f570a83c960bda6
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-11 21:14:09 -0300

    Deleted login route

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-const { userlist, newUser, loginUser, userById } = require('../Controllers/Users')
+const { userlist, newUser, userById } = require('../Controllers/Users')
 
 // GET
 router.get('/', userlist);
@@ -9,6 +9,5 @@ router.get('/userId/:uid', userById)
 
 // POST
 router.post('/register', newUser);
-router.post('/login', loginUser)
 
 module.exports = router
